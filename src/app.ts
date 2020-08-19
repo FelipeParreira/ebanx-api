aabd7fa707e9fd2f504ab537cf866f687b1c7b56
Felipe Parreira <felipeparreirafb@gmail.com>
2020-08-19 09:28:34 -0300

    docs: add swagger docs

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,17 @@
+import swaggerUI from 'swagger-ui-express';
 import bodyParser from 'body-parser';
 import express from 'express';
 import helmet from 'helmet';
 import morgan from 'morgan';
 import cors from 'cors';
 
+import swaggerDocument from '../docs/swagger.json';
+
 import router from './routes';
 
 const app = express();
+
+// middleware
 
 // Security HTTP headers
 // See https://helmetjs.github.io/docs/
@@ -17,6 +22,10 @@ app.use(morgan('tiny', { skip: () => process.env.NODE_ENV === 'test' }));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+// docs
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument));
+
+// routes
 app.use('/', router);
 
 export default app;
